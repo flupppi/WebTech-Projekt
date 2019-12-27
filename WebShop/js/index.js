f962a7ea58b8db115edbf33204796a304a1d23a9
flupppi <kalchscf@hochschule-trier.de>
2019-12-27 04:44:59 +0100

    Keyboard navigierbarkeit verbessert.
    Klarere Seitenstruktur, Header und Footer sind komplett tastaturnavigierbar.
    Animierte outline bei Tab

---
+++ WebShop/js/index.js
@@ -0,0 +1,8 @@
+function mehr() {
+    document.getElementById("mehr").innerHTML = "<ul class=\"w3-ul\">\n" +
+        "                <li><a class=\"w3-hover-black\">T-Shirts</a></li>\n" +
+        "                <li><a class=\"w3-hover-black\">Hoodies & Sweatshirts</a></li>\n" +
+
+        "            </ul>";
+    
+}
