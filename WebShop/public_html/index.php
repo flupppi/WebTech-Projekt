ccd7756e450e66b8b102894d18ad2fd2ad8d1b78
flupppi <kalchscf@hochschule-trier.de>
2020-01-07 13:32:03 +0100

    PHP
    Header und Footer können mit PHP eingebunden werden

--- WebShop/public_html/index.php
+++ WebShop/public_html/index.php
@@ -1,12 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Home</title>
+<link rel="shortcut icon" type="image/x-icon" href="img/31714.png">
+
+<script rel="script" href="index.js"></script>
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<body>
 <?php
-    include "header.php"
+    include "header.php";
+    include "index.php";
+    include "footer.php";
 ?>
-<main>
-    <?php
-        if (isset($_SESSION['userId'])) {
-            echo '<p class=login-status">You are logged in!</p>';
-        }else{
-            echo '<p class=login-status">You are logged out!</p>';
-        }
-    ?>
-</main>
+</body>
+
+</html>
