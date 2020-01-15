b66b5748b34a878d533893d52701bf3675188b12
flupppi <kalchscf@hochschule-trier.de>
2020-01-15 08:51:09 +0100

    Blogübersicht aktualisiert
    Blogübersicht mit neuen Inhalten bestückt

--- WebShop/public_html/Blogübersicht.php
+++ WebShop/public_html/Blogübersicht.php
@@ -9,6 +9,8 @@ include "header.php";
         <header class=" w3-border-bottom w3-border-light-gray">
             <h1 class="w3-xlarge" style="font-weight: bold">BLOG</h1>
         </header>
+
+
         <article class="w3-border-bottom w3-border-light-gray w3-margin w3-white w3-row">
             <header class="w3-container">
                 <h2 class="w3-xlarge w3-bold">Fair Fashion Guide</h2>
@@ -16,123 +18,140 @@ include "header.php";
             </header>
 
             <section class="w3-twothird w3-container">
-                <p>Grafik: future fashion forward e. V. <br>
-                    Countdown<br>
-<br>
-                    In 8 Tagen, also am 24. April, startet die Fashion Revolution Week weltweit in Memorian an den
-                    Einsturz der Rana Plaza-Textilfabriken 2013 in Bangladesch. Damals wurden über 1138 Menschen unter
-                    den Trümmern begraben, rund 2500 wurden zum Teil schwer verletzt. Damit so etwas nie wieder passiert
-                    macht die Fashion Revolution-Bewegungen jedes Jahr zum Jahrestag Aktionen in der ganzen Welt…
-
+                <h4 class="w3-large w3-bold">„Trends ändern sich, Persönlichkeit und Attitude bleiben“</h4>
+                <br>
+                Experten-Interviews mit Martina Glomb, Marie Nasemann, Kirsten Brodde, Max Gilgenmann und Magdalena
+                Schaffrin sowie Tipps zum fairen Modekonsum aus dem Fair Fashion Guide. Webseite und Printmagazin sind
+                in Zusammenarbeit mit FEMNET e. V. und dem Beneficial Design Institute entstanden. Hier ein Auszug mit
+                Links zum Weiterschmökern im Original…
                 <br></p>
                 <nav class="w3-row w3-col m8 s12">
                     <p>
                         <button class="w3-button w3-padding-large
-                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE »</a></button>
+                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE
+                                »</a></button>
+                    </p>
+                </nav>
+
+            </section>
+            <section class="w3-third">
+                <img class="w3-image" src="../text/blog/fair_fashion_guide.jpg" alt="future fashion forward e. V.">
+                <figcaption class="w3-small">Trendy und nachhaltig: Wie das geht zeigt der Fair Fashion Guide.
+                </figcaption>
+            </section>
+        </article>
+
+
+        <article class="w3-border-bottom w3-border-light-gray w3-margin w3-white w3-row">
+            <header class="w3-container">
+                <h2 class="w3-xlarge w3-bold">Fashion Revolution Week</h2>
+                <h3 class="w3-large">ekoehrer, <span class="w3-opacity">16/04/2017</span></h3>
+            </header>
+
+            <section class="w3-twothird w3-container">
+                <h4 class="w3-large w3-bold">Countdown</h4>
+                <p class="w3-padding">
+
+                    In 8 Tagen, also am 24. April, startet die Fashion Revolution Week weltweit in Memorian an den
+                    Einsturz der Rana Plaza-Textilfabriken 2013 in Bangladesch.<br> Damals wurden über 1138 Menschen
+                    unter
+                    den Trümmern begraben, rund 2500 wurden zum Teil schwer verletzt. Damit so etwas nie wieder passiert
+                    macht die Fashion Revolution-Bewegungen jedes Jahr zum Jahrestag Aktionen in der ganzen Welt…
+
+                </p>
+                <nav class="w3-row w3-col m8 s12">
+                    <p>
+                        <button class="w3-button w3-padding-large
+                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE
+                                »</a></button>
                     </p>
                 </nav>
             </section>
             <section class="w3-third">
-                <img class="w3-image" src="../text/blog/fair_fashion_guide.jpg" alt="future fashion forward e. V.">
+                <img class="w3-image" src="../text/blog/quote_ellenköhrer.jpg" alt="future fashion forward e. V.">
                 <figcaption class="w3-hide">future fashion forward e. V.</figcaption>
+            </section>
+        </article>
+
+
+        <article class="w3-border-bottom w3-border-light-gray w3-margin w3-white w3-row">
+            <header class="w3-container">
+                <h2 class="w3-xlarge w3-bold">Interview</h2>
+                <h3 class="w3-large">ekoehrer, <span class="w3-opacity">03/04/2017</span></h3>
+            </header>
+
+            <section class="w3-twothird w3-container">
+                <h4 class="w3-large w3-bold">MUD JEANS Blog: Interview with Ellen Köhrer</h4>
+                <p class="w3-padding">
+
+                    Fashion Made Fair’ is a book regarding ethical and eco-friendly fashion labels. The authors of the
+                    book, journalist Ellen Köhrer and Magdalena Schaffrin, founder of the Greenshowroom for sustainable
+                    high-end fashion, put emphasis on presenting sustainable fashion in a modern way. The book is an
+                    excellent selection of 33 designers and labels from all over the world that put equal emphasis on
+                    sustainability as creating well-designed clothing.
+                    The book lets you discover fair trade and sustainable fashion labels from more established brands
+                    like Vivienne Westwood, Freitag and Patagonia, but it also features a range of exciting younger
+                    fashion labels. The book itself features well-written pages, including expert talks explaining all
+                    aspects of sustainability, in combination with the beautiful imagery. Excited as we were to see
+                    several pages dedicated to Mud Jeans, we set up an interview with author Ellen Köhrer which you will
+                    find below…
+
+                </p>
+                <nav class="w3-row w3-col m8 s12">
+                    <p>
+                        <button class="w3-button w3-padding-large
+                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE
+                                »</a></button>
+                    </p>
+                </nav>
+            </section>
+            <section class="w3-third">
+                <img class="w3-image w3-sha" src="../text/blog/MUD-JEANS-Blog_Interview-moi.jpg"
+                     alt="future fashion forward e. V.">
+                <figcaption class="w3-hide">FASHION MADE FAIR</figcaption>
             </section>
         </article>
 
         <article class="w3-border-bottom w3-border-light-gray w3-margin w3-white w3-row">
             <header class="w3-container">
-                <h2 class="w3-xlarge w3-bold">Fair Fashion Guide</h2>
-                <h3 class="w3-large">ekoehrer, <span class="w3-opacity">27/08/2017</span></h3>
+                <h2 class="w3-xlarge w3-bold">Fashion Week Berlin</h2>
+                <h3 class="w3-large">ekoehrer, <span class="w3-opacity">10/01/2017</span></h3>
             </header>
 
             <section class="w3-twothird w3-container">
-                <p>Grafik: future fashion forward e. V. <br>
-                    Countdown<br>
-                    <br>
+
+                <h4 class="w3-large w3-bold">Fashion Week Berlin
+                </h4>
+                <p class="w3-padding">
+
                     In 8 Tagen, also am 24. April, startet die Fashion Revolution Week weltweit in Memorian an den
                     Einsturz der Rana Plaza-Textilfabriken 2013 in Bangladesch. Damals wurden über 1138 Menschen unter
                     den Trümmern begraben, rund 2500 wurden zum Teil schwer verletzt. Damit so etwas nie wieder passiert
                     macht die Fashion Revolution-Bewegungen jedes Jahr zum Jahrestag Aktionen in der ganzen Welt…
 
-                    <br></p>
+                </p>
                 <nav class="w3-row w3-col m8 s12">
                     <p>
                         <button class="w3-button w3-padding-large
-                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE »</a></button>
+                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE
+                                »</a></button>
                     </p>
                 </nav>
             </section>
             <section class="w3-third">
-                <img class="w3-image" src="../text/blog/fair_fashion_guide.jpg" alt="future fashion forward e. V.">
-                <figcaption class="w3-hide">future fashion forward e. V.</figcaption>
-            </section>
-        </article>
-
-        <article class="w3-border-bottom w3-border-light-gray w3-margin w3-white w3-row">
-            <header class="w3-container">
-                <h2 class="w3-xlarge w3-bold">Fair Fashion Guide</h2>
-                <h3 class="w3-large">ekoehrer, <span class="w3-opacity">27/08/2017</span></h3>
-            </header>
-
-            <section class="w3-twothird w3-container">
-                <p>Grafik: future fashion forward e. V. <br>
-                    Countdown<br>
-                    <br>
-                    In 8 Tagen, also am 24. April, startet die Fashion Revolution Week weltweit in Memorian an den
-                    Einsturz der Rana Plaza-Textilfabriken 2013 in Bangladesch. Damals wurden über 1138 Menschen unter
-                    den Trümmern begraben, rund 2500 wurden zum Teil schwer verletzt. Damit so etwas nie wieder passiert
-                    macht die Fashion Revolution-Bewegungen jedes Jahr zum Jahrestag Aktionen in der ganzen Welt…
-
-                    <br></p>
-                <nav class="w3-row w3-col m8 s12">
-                    <p>
-                        <button class="w3-button w3-padding-large
-                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE »</a></button>
-                    </p>
-                </nav>
-            </section>
-            <section class="w3-third">
-                <img class="w3-image" src="../text/blog/fair_fashion_guide.jpg" alt="future fashion forward e. V.">
-                <figcaption class="w3-hide">future fashion forward e. V.</figcaption>
-            </section>
-        </article>
-
-        <article class="w3-border-bottom w3-border-light-gray w3-margin w3-white w3-row">
-            <header class="w3-container">
-                <h2 class="w3-xlarge w3-bold">Fair Fashion Guide</h2>
-                <h3 class="w3-large">ekoehrer, <span class="w3-opacity">27/08/2017</span></h3>
-            </header>
-
-            <section class="w3-twothird w3-container">
-                <p>Grafik: future fashion forward e. V. <br>
-                    Countdown<br>
-                    <br>
-                    In 8 Tagen, also am 24. April, startet die Fashion Revolution Week weltweit in Memorian an den
-                    Einsturz der Rana Plaza-Textilfabriken 2013 in Bangladesch. Damals wurden über 1138 Menschen unter
-                    den Trümmern begraben, rund 2500 wurden zum Teil schwer verletzt. Damit so etwas nie wieder passiert
-                    macht die Fashion Revolution-Bewegungen jedes Jahr zum Jahrestag Aktionen in der ganzen Welt…
-
-                    <br></p>
-                <nav class="w3-row w3-col m8 s12">
-                    <p>
-                        <button class="w3-button w3-padding-large
-                                        w3-white w3-border"><a href="Blogseite.php" class="w3-nouline w3-bold">READ MORE »</a></button>
-                    </p>
-                </nav>
-            </section>
-            <section class="w3-third">
-                <img class="w3-image" src="../text/blog/fair_fashion_guide.jpg" alt="future fashion forward e. V.">
-                <figcaption class="w3-hide">future fashion forward e. V.</figcaption>
+                <img class="w3-image" src="../text/blog/kronprinzenpalaisswmoi.jpg" alt="future fashion forward e. V.">
+                <figcaption class="w3-hide">Das war einmal: Greenshowroom Salonshow im Kronprinzenpalais</figcaption>
             </section>
         </article>
     </main>
     <nav class="w3-center w3-padding-24">
         <ul class="w3-bar w3-border w3-round">
-            <a href="#" class="w3-button">&laquo;</a>
-            <a href="#" class="w3-button">1</a>
-            <a href="#" class="w3-button">2</a>
-            <a href="#" class="w3-button">3</a>
-            <a href="#" class="w3-button">4</a>
-            <a href="#" class="w3-button">&raquo;</a>
+            <a href="Blogübersicht.php" class="w3-button">&laquo;</a>
+            <a href="Blogübersicht.php" class="w3-button">1</a>
+            <a href="Blogübersicht.php" class="w3-button">2</a>
+            <a href="Blogübersicht.php" class="w3-button">3</a>
+            <a href="Blogübersicht.php" class="w3-button">4</a>
+            <a href="Blogübersicht.php" class="w3-button">&raquo;</a>
         </ul>
     </nav>
 
