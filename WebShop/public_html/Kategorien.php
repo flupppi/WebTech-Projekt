565f815c24e351dd209b5b22c6ce7edd10999099
leonard-ruebel <58036276+leonard-ruebel@users.noreply.github.com>
2020-01-15 22:47:54 +0100

    kategorien neu
    neue kategorien page inhalte müssen noch geladen werten und die responsivness kann noch verbessert werden

--- WebShop/public_html/Kategorien.php
+++ WebShop/public_html/Kategorien.php
@@ -2,57 +2,49 @@
 
 include "header.php";
 ?>
-    <!--Breadcrumb-->
-    <div class="w3-white">
-        <div class="w3-bar" style="max-width: 1280px; margin:auto;">
-            <ul class="breadcrumb w3-white">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="#">Kategorien</a></li>
 
-                <i class="fab fa-youtube w3-right"></i>
-                <i class="fab fa-instagram w3-right"></i>
-                <i class="fab fa-facebook w3-right"></i>
-                <select class="w3-select w3-right" name="option" style="size: 200px">
-                    <option disabled selected value="">Choose your option</option>
-                    <option value="1">Preis desc</option>
-                    <option value="2">Preis asc</option>
-                    <option value="3">Lexikographisch asc</option>
-                    <option value="3">Lexikographisch desc</option>
-                </select>
-            </ul>
+<main>
+    <!--div class="w3-panel" style="padding:50px"-->
+        <div class="w3-content-margin" style="padding: 50px">
+            <div class="w3-row w3-padding-48">
+                <div class="w3-col s5 w3-card-4 w3-left w3-mobile w3-display-container"
+                     onmouseover="changeImg('r1p1', 'img/deluxe_polo_mint_white_large.jpg')"
+                     onmouseout="changeImg('r1p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <a href="Produktübersicht.php" class=""> <img id="r1p1" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
+                </div>
+                <div class="w3-col s5 w3-card-4 w3-right w3-mobile w3-display-container"
+                     onmouseover="changeImg('r1p2', 'img/deluxe_polo_mint_white_large.jpg')"
+                     onmouseout="changeImg('r1p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <a href="Produktübersicht.php" class=""> <img id="r1p2" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
+                </div>
+            </div>
+            <div class="w3-row w3-padding-48">
+                <div class="w3-col s5 w3-card-4 w3-left w3-mobile w3-display-container"
+                     onmouseover="changeImg('r2p1', 'img/deluxe_polo_mint_white_large.jpg')"
+                     onmouseout="changeImg('r2p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <a href="Produktübersicht.php" class=""> <img id="r2p1" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
+                </div>
+                <div class="w3-col s5 w3-card-4 w3-right w3-mobile w3-display-container"
+                     onmouseover="changeImg('r2p2', 'img/deluxe_polo_mint_white_large.jpg')"
+                     onmouseout="changeImg('r2p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <a href="Produktübersicht.php" class=""> <img id="r2p2" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
+                </div>
+            </div>
 
-
-        </div>
     </div>
-    </header>
-    <!-- TODO content alginment-->
 
+</main>
 
-    <main class="w3-container w3-padding-48">
-        <div class="w3-row w3-padding-16" style="max-width: 1280px; margin: auto;">
-            <!--Pick your gender either men, women or child but we don't target childs as customers-->
-            <div class="w3-col l2 w3-light-gray w3-card w3-hide-small" id="memes">
-                <ul class="w3-ul">
-                    <li><a class="w3-hover-black">Mens</a></li>
-                    <li><a class="w3-hover-black">Womes</a></li>
-                </ul>
-            </div>
-            <div class="w3-center" id="content" style="z-index: -1">
-                <img alt="womens section" class="w3-col w3-container s12 m3 l5 " id="women"
-                     onclick="changeWomens()" src="img/PW_Gender_Page_Womens_967X813_DE.jpg">
-                <img alt="mens section" class="w3-col w3-container s12 m3 l5 " id="men"
-                     onclick="changeMens()" src="img/PW_Gender_Page_Mens_967X813_DE.jpg">
-            </div>
+<script>
+function changeImg(id, path){
+    document.getElementById(id).src=path;
+}
+</script>
 
-            <!-- pagination -->
-            <div class="w3-center w3-padding-24" id="pagination">
-
-            </div>
-        </div>
-        <!-- TODO txt database -->
-        <!-- TODO categories all products and add attributes for the same -->
-        <!-- TODO create functions to sort the same according to their attributes-->
-    </main>
 
 <?php
 include "footer.php";
