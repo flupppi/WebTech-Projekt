59ff7197a5b61c59ae325aa3e5cb8e484f8d9768
leonard-ruebel <58036276+leonard-ruebel@users.noreply.github.com>
2020-01-16 21:39:56 +0100

    header kommentiert und änderungen für validator
    der validator zeig immernoch errors an aber die sind größtenteils wegen dem php

--- WebShop/public_html/header.php
+++ WebShop/public_html/header.php
@@ -26,8 +26,6 @@
         border-color:aqua;
         border-width: 2px;
         border-radius: 5px;
-
-
     }
 
     :focus:not(.focus-ring) {
@@ -41,55 +39,50 @@
 <section id="header" class="w3-hide-small">
     <!--Top Bar - Language Switch-->
     <header id="top" class="w3-top">
-        <nav role="menubar" class="w3-bar w3-white w3-center"> Webshop
-
-
-            <nav role="button" class="w3-right">
-                <nav role="button" tabindex="0" class="w3-button">Deutsch</nav>
-                <nav role="button" tabindex="0" class="w3-button">English</nav>
+        <nav class="w3-bar w3-white w3-center">
+           <nav class="w3-right">
+                <nav tabindex="0" class="w3-button">Deutsch</nav>
+                <nav tabindex="0" class="w3-button">English</nav>
             </nav>
         </nav>
 
         <!--Static Icon Bar-->
-
-        <nav role="menubar" class="w3-bar w3-white">
-
-            <form role="searchbox" action="/action_page.php"
+        <nav class="w3-bar w3-white">
+            <form action="/action_page.php"
                   class="w3-hover-white w3-border-light-grey w3-left w3-margin-left">
                 <label>
                     <input class="w3-input w3-bar-item" type="text" placeholder="Search.." name="search" id="search">
                 </label>
                 <button class="w3-button w3-green" type="submit"><i class="fa fa-search"></i></button>
-
             </form>
-            <nav role="button" class="w3-right">
-                <a onclick="showSignup('signup')" role="button" tabindex="0" class=" w3-button"> <i class="fa fa-user"></i>
+            <nav class="w3-right">
+                <a onclick="showSignup('signup')" tabindex="0" class=" w3-button"> <i class="fa fa-user"></i>
                     ACCOUNT
                     ERSTELLEN
                 </a>
-
-
-                <button onclick="showCart('cart')" role="button" tabindex="0" class=" w3-button "><i
+                <button onclick="showCart('cart')" tabindex="0" class=" w3-button "><i
                         class="fa fa-shopping-basket"></i>
                     WARENKORB</button>
 
+                <!--shows login when not signed in and shows logout when signed in-->
                 <?php if(ISSET($_SESSION['userId'])){
-                    echo '<form action="../includes/logout.inc.php"><button type="submit"  role="button" tabindex="0" class=" w3-button"><i
+                    echo '<form action="../includes/logout.inc.php"><button type="submit" tabindex="0" class=" w3-button"><i
                         class="fa fa-sign-out"></i>
                     LOGOUT</button></form>';}else{
-                    echo '<button onclick="showLogin(\'login\')" role="button" tabindex="0" class=" w3-button"><i
+                    echo '<button onclick="showLogin(\'login\')" tabindex="0" class=" w3-button"><i
                         class="fa fa-sign-in"></i>
                     LOGIN</button>';
                 }?>
             </nav>
         </nav>
-
     </header>
+
     <!--Site Logo-->
     <figure class="w3-center w3-big-margin-top w3-white w3-hide-small w3-padding-32">
         <img src="img/nature.png" alt="webshop logo image" style="width:180px">
         <figcaption class="w3-text-gray">Shop for sustainable Clothing</figcaption>
     </figure>
+    
     <!--Navigation Bar-->
     <header class="w3-light-gray w3-padding w3-hide-small">
         <nav class="w3-bar w3-light-gray w3-center w3-medium w3-content-margin">
@@ -106,7 +99,7 @@
 
 
 <!--MOBILE HEADER HTML-->
-<section id="header" class="w3-hide-medium w3-hide-large">
+<section id="mobile-header" class="w3-hide-medium w3-hide-large">
     <!--Top Bar - Language Switch-->
     <header class="w3-top">
         <nav class="w3-bar w3-white w3-center"> Webshop</nav>
@@ -118,10 +111,10 @@
             <button class="w3-button" onclick="myFunction()">
                 <i class="fa fa-bars"></i>
             </button>
-            <nav  id="demo"class="w3-dropdown-content w3-bar-block w3-card-4">
+            <nav  id="demo" class="w3-dropdown-content w3-bar-block w3-card-4">
                 <form  role="search" action="#" class="w3-bar  w3-hover-white w3-border-light-grey  ">
                     <label>
-                        <input class="w3-input w3-bar-item" type="text" placeholder="Search.." name="search" id="search">
+                        <input class="w3-input w3-bar-item" type="text" placeholder="Search.." name="search" id="mobile-search">
                     </label>
                     <button class="w3-bar-item w3-button w3-green" type="submit"><i class="fa fa-search"></i></button>
 
@@ -132,16 +125,16 @@
                 <a class="w3-bar-item   w3-button" href="About.php">ABOUT</a>
                 <a class="w3-bar-item   w3-button" href="Infoseite.php">INFOSEITE</a>
                 <a class="w3-bar-item   w3-button" href="FAQ.php">FAQ</a>
-                <nav role="navigation">
-                    <a role="link" href="" tabindex="0" class="w3-button">Deutsch</a>
-                    <a role="link" href="" tabindex="0" class="w3-button">English</a>
+                <nav>
+                    <a href="" tabindex="0" class="w3-button">Deutsch</a>
+                    <a href="" tabindex="0" class="w3-button">English</a>
                 </nav>
             </nav>
 
 
-            <button onclick="showSignup('signup')" role="button" tabindex="0" class=" w3-button "><i class="fa fa-user"></i>
+            <button onclick="showSignup('signup')" tabindex="0" class=" w3-button "><i class="fa fa-user"></i>
             </button>
-            <button onclick="showCart('cart')" role="button" tabindex="0" class=" w3-button  "><i
+            <button onclick="showCart('cart')" tabindex="0" class=" w3-button  "><i
                     class="fa fa-shopping-basket"></i>
             </button>
           <?php if(!ISSET($_SESSION['userId'])){
@@ -150,7 +143,7 @@
                     </button>';
           }else {
               echo '<form action="../includes/logout.inc.php">
-                    <button  type="submit" role="button" tabindex="0" class=" w3-button "><i
+                    <button  type="submit" tabindex="0" class=" w3-button "><i
                     class="fa fa-sign-out"></i>
             </button></form>';
           }
