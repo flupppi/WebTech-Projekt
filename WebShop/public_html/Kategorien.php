dc2b51a328a5e1b8b3184bd456d4d377025f8ced
leonard-ruebel <58036276+leonard-ruebel@users.noreply.github.com>
2020-01-16 14:17:09 +0100

    kategorien verbesserung

--- WebShop/public_html/Kategorien.php
+++ WebShop/public_html/Kategorien.php
@@ -2,42 +2,54 @@
 
 include "header.php";
 ?>
+<!DOCTYPE html>
+<html>
 
 <main>
-    <!--div class="w3-panel" style="padding:50px"-->
-        <div class="w3-content-margin" style="padding: 50px">
-            <div class="w3-row w3-padding-48">
-                <div class="w3-col s5 w3-card-4 w3-left w3-mobile w3-display-container"
-                     onmouseover="changeImg('r1p1', 'img/deluxe_polo_mint_white_large.jpg')"
-                     onmouseout="changeImg('r1p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+    <nav style="padding: 4vw">
+        <section class="w3-row">
+            <div class="w3-half">
+                <div class="w3-card-4 w3-display-container"
+                    style="margin: 4vw"
+                    onmouseover="changeImg('r1p1', 'img/deluxe_polo_mint_white_large.jpg')"
+                    onmouseout="changeImg('r1p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-text-white" style="font-size:8vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r1p1" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
-                <div class="w3-col s5 w3-card-4 w3-right w3-mobile w3-display-container"
-                     onmouseover="changeImg('r1p2', 'img/deluxe_polo_mint_white_large.jpg')"
-                     onmouseout="changeImg('r1p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+            </div>
+            <div class="w3-half">
+                <div class="w3-card-4 w3-display-container"
+                    style="margin: 4vw"
+                    onmouseover="changeImg('r1p2', 'img/deluxe_polo_mint_white_large.jpg')"
+                    onmouseout="changeImg('r1p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-text-white" style="font-size:8vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r1p2" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
             </div>
-            <div class="w3-row w3-padding-48">
-                <div class="w3-col s5 w3-card-4 w3-left w3-mobile w3-display-container"
-                     onmouseover="changeImg('r2p1', 'img/deluxe_polo_mint_white_large.jpg')"
-                     onmouseout="changeImg('r2p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+        </section>
+        <section class="w3-row">
+            <div class="w3-half">
+                <div class="w3-card-4 w3-display-container"
+                    style="margin: 4vw"
+                    onmouseover="changeImg('r2p1', 'img/deluxe_polo_mint_white_large.jpg')"
+                    onmouseout="changeImg('r2p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-text-white" style="font-size:8vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r2p1" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
-                <div class="w3-col s5 w3-card-4 w3-right w3-mobile w3-display-container"
-                     onmouseover="changeImg('r2p2', 'img/deluxe_polo_mint_white_large.jpg')"
-                     onmouseout="changeImg('r2p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+            </div>
+            <div class="w3-half">
+                <div class="w3-card-4 w3-display-container"
+                    style="margin: 4vw"
+                    onmouseover="changeImg('r2p2', 'img/deluxe_polo_mint_white_large.jpg')"
+                    onmouseout="changeImg('r2p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
+                    <div class="w3-display-middle w3-text-white" style="font-size:8vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r2p2" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
             </div>
-
-    </div>
-
+        </section>
+    </nav>
 </main>
+</html>
 
 <script>
 function changeImg(id, path){
