8450157c44e21991d5b5579fa61a844a42e65ab3
leonard-ruebel <58036276+leonard-ruebel@users.noreply.github.com>
2020-01-16 10:19:07 +0100

    kategorien responsive verbesserung

--- WebShop/public_html/Kategorien.php
+++ WebShop/public_html/Kategorien.php
@@ -10,13 +10,13 @@ include "header.php";
                 <div class="w3-col s5 w3-card-4 w3-left w3-mobile w3-display-container"
                      onmouseover="changeImg('r1p1', 'img/deluxe_polo_mint_white_large.jpg')"
                      onmouseout="changeImg('r1p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <div class="w3-display-middle w3-text-white" style="font-size:9vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r1p1" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
                 <div class="w3-col s5 w3-card-4 w3-right w3-mobile w3-display-container"
                      onmouseover="changeImg('r1p2', 'img/deluxe_polo_mint_white_large.jpg')"
                      onmouseout="changeImg('r1p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <div class="w3-display-middle w3-text-white" style="font-size:9vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r1p2" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
             </div>
@@ -24,13 +24,13 @@ include "header.php";
                 <div class="w3-col s5 w3-card-4 w3-left w3-mobile w3-display-container"
                      onmouseover="changeImg('r2p1', 'img/deluxe_polo_mint_white_large.jpg')"
                      onmouseout="changeImg('r2p1', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <div class="w3-display-middle w3-text-white" style="font-size:9vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r2p1" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
                 <div class="w3-col s5 w3-card-4 w3-right w3-mobile w3-display-container"
                      onmouseover="changeImg('r2p2', 'img/deluxe_polo_mint_white_large.jpg')"
                      onmouseout="changeImg('r2p2', 'img/deluxe_shirt_black_tuerkis_large.jpg')">
-                    <div class="w3-display-middle w3-jumbo w3-text-white"> MIDDLE</div>
+                    <div class="w3-display-middle w3-text-white" style="font-size:9vw"> MIDDLE</div>
                     <a href="Produktübersicht.php" class=""> <img id="r2p2" src="img/deluxe_shirt_black_tuerkis_large.jpg" style="width:100%"> </a>
                 </div>
             </div>
